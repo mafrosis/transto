cf6bc0c0fe0e7b6500c3d4aeefc622795fd8f860
Matt Black <github@mafro.net>
2024-03-07 05:51:11 +1100

    NAB import compatibility

--- transto/nab.py
+++ transto/nab.py
@@ -18,20 +18,8 @@ def nab(file: io.BufferedReader):
     df.rename(columns={'Amount': 'amount'}, inplace=True)
 
     # Drop useless columns
-    df.drop(columns=['Date', 'Account Number', 'Unnamed: 3', 'Transaction Details', 'Balance', 'Category', 'Merchant Name'], inplace=True)
+    df.drop(columns=['Date', 'Account Number', 'Unnamed: 3', 'Transaction Details', 'Transaction Type', 'Balance', 'Category', 'Merchant Name'], inplace=True)
 
     df = match(df)
 
-    # Handling for cash advance
-    df.loc[df['Transaction Type'] == 'CREDIT CARD CASH ADVANCE', ['topcat', 'seccat', 'searchterm']] = ['bills', 'cash', 'CREDIT CARD CASH ADVANCE']
-
-    # Handle payments
-    df.loc[df['Transaction Type'] == 'CREDIT CARD PAYMENT', ['topcat', 'seccat', 'searchterm']] = ['payment', 'payment', 'CREDIT CARD PAYMENT']
-
-    # Handle fees
-    df.loc[df['Transaction Type'].str.contains('FEES|DEBIT ADJUSTMENTS|MISCELLANEOUS CREDIT'), ['topcat', 'seccat', 'searchterm']] = ['bills', 'bankfees', 'FEES']
-
-    # Drop Transaction Type now it's finished with
-    df = df.drop(columns=['Transaction Type'])
-
-    commit(df, 'NAB', 'transactions')
+    commit(df, 'NAB', 'credit')
